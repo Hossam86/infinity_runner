8b0d932d80c7ac5f708fa5e2c03712c751016998
hossam samir <hossamsamir@outlook.com>
2023-09-12 19:20:30 +0300

    disable  and enable player input actions

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -4,10 +4,33 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    PlayerInput input;
+
+    private void OnEnable()
+    {
+        input.Enable();
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (input == null)
+        {
+            input =new PlayerInput();
+        }
+
+        input.GamePlay.Move.performed += Move_performed;
+    }
+
+    private void Move_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        throw new System.NotImplementedException();
     }
 
     // Update is called once per frame
